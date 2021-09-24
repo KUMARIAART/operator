56ec108265db085bd15063573cb549d1389175f0
AARTI KUMARI <89918154+KUMARIAART@users.noreply.github.com>
2021-09-24 16:24:00 +0530

    Add files via upload

---
+++ kl.py
@@ -0,0 +1,9 @@
+"python program to enter the length in centimeter and convert it into meter and kilometer."
+
+print("enter the length in centimeter::")
+centimeter,meter,kilomiter=float(input()),0,0
+meter=(float)(centimeter/100)
+kilomiter=(float)(centimeter/100000)
+print("length in meter=",meter,"meter")
+print("length in kilometer=",kilomiter,"kilometer")
+
